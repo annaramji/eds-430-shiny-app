71006abb47dbc43e8a4bc909450d3975ea6c6be6
annaramji <anna@ramji.org>
2024-01-26 11:34:21 -0800

    made interactive penguins body mass single app

---
+++ scratch/practice-script-app1.R
@@ -0,0 +1,49 @@
+# drafting objects for single-file-app 
+
+
+# load packages ----
+library(palmerpenguins)
+library(tidyverse)
+
+# practice filtering data outside before bringing it into shiny
+
+# filter penguins df for obs where body mass is >= 3000 & <= 4000 ----
+body_mass_df <- penguins |> 
+  filter(body_mass_g %in% c(3000:4000)) # this is what you'd want to be update-able
+
+
+# create scatterplot ----
+# for outputId bodyMass_scatter_plot, renderPlot ({ this code })
+ggplot(na.omit(body_mass_df), 
+       aes(x = flipper_length_mm, y = bill_length_mm, 
+           color = species, shape = species)) +
+  geom_point() +
+  # adding Species name assignment to colors and shapes to avoid auto-assigning at selective ranges (i.e., Gentoo becomes orange circle at 5-6000 range, which is confusing/bad for user)
+  scale_color_manual(values = c("Adelie" = "darkorange", "Chinstrap" = "purple", "Gentoo" = "cyan4")) +
+  scale_shape_manual(values = c("Adelie" = 19, "Chinstrap" = 17, "Gentoo" = 15)) +
+  labs(x = "Flipper length (mm)", y = "Bill length (mm)", 
+       color = "Penguin species", shape = "Penguin species") +
+  theme_minimal() +
+  theme(legend.position = c(0.85, 0.2),
+        legend.background = element_rect(color = "white"))
+
+
+# live-coding
+
+ggplot(na.omit(penguins),
+       aes(x = flipper_length_mm,
+           y = bill_length_mm,
+           color = species,
+           shape = species)) +
+  geom_point() +
+  # add Allison's colors
+  scale_color_manual(values = c("darkorange", "purple", "cyan4")) +
+  scale_shape_manual(values = c(19, 17, 15)) +
+  labs(x = "Flipper length (mm)", y = "Bill length (mm)",
+       color = "Penguin species", shape = "Penguin species") +
+  theme_minimal() +
+  # move legend and update background color
+  
+  theme(legend.position = c(0.85, 0.2), # x,y coordinate to move the legend (can also say top, bottom, corner, etc.)
+        legend.background = element_rect(color = "white")
+        )
--- single-file-app/app.R
+++ single-file-app/app.R
@@ -1,16 +1,78 @@
 # load packages
 library(shiny)
+library(palmerpenguins)
+library(tidyverse)
 
 # user interface ----
 # standard naming convention
 # fluidPage creates the user interface that allows people to view it in the browser, does stuff under the hood. If we want to see stuff rendered in our UI, we'll need to add it in fluidPage
-ui <- fluidPage()
+ui <- fluidPage(
+  
+  # app title ----
+  # update styling with parallel common HTML tags
+  # style our text by adding static HTML elements using tags – a list of functions that parallel common HTML tags (e.g. <h1> == tags$h1()) The most common tags also have wrapper functions (e.g. h1()).
+  tags$h1("My App Title") ,
+  
+  # app subtitle ----
+  h4(strong("Exploring Antarctic Penguin Data")), # alternatively, `tags$h4(tags$strong("text"))
+  
+  # body mass slider input ----
+  sliderInput(inputId = "body_mass_input", 
+              label = "Select a range of body masses (g):",
+              min = 2700,
+              max = 6300,
+              # value = initial value(s) of the slider
+              value = c(3000, 4000)),
+  
+  # body mass slider output ----
+  plotOutput(outputId = "bodyMass_scatterplot_output")
+  
+)
 
 
 # server instructions ----
 # standard naming convention
 # function takes inputs and outputs, we'll define these later
-server <- function(input, output) {}
+server <- function(input, output) {
+  # filter body masses
+  # reactive dataframe - based on inputs taht are supplied
+  body_mass_df <- reactive({
+    
+    penguins |> 
+      filter(body_mass_g %in% c(input$body_mass_input[1]:input$body_mass_input[2])) # first value of slider, second value of slider
+    
+    
+  })
+  
+  
+  # render penguin body mass scatter plot ----
+  output$bodyMass_scatterplot_output <- renderPlot({
+    
+    # code to generate scatterplot here
+           # our reactive df  followed by ()
+    ggplot(na.omit(body_mass_df()),
+           aes(x = flipper_length_mm,
+               y = bill_length_mm,
+               color = species,
+               shape = species)) +
+      geom_point() +
+      # add Allison's colors
+      # adding Species name assignment to colors and shapes to avoid auto-assigning at selective ranges (i.e., Gentoo becomes orange circle at 5-6000 range, which is confusing/bad for user)
+      scale_color_manual(values = c("Adelie" = "darkorange", "Chinstrap" = "purple", "Gentoo" = "cyan4")) +
+      scale_shape_manual(values = c("Adelie" = 19, "Chinstrap" = 17, "Gentoo" = 15)) +
+      labs(x = "Flipper length (mm)", y = "Bill length (mm)",
+           color = "Penguin species", shape = "Penguin species") +
+      theme_minimal() +
+      # move legend and update background color
+      
+      theme(legend.position = c(0.85, 0.2), # x,y coordinate to move the legend (can also say top, bottom, corner, etc.)
+            legend.background = element_rect(color = "white")
+      )
+    
+  }
+  )
+  
+}
 
 
 # combine UI & server into an app ----
