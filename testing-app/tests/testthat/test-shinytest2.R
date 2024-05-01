688cba08c9e610dcbd4ae012ad6c9a8a4ecd7812
annaramji <anna@ramji.org>
2024-05-01 14:33:15 -0700

    adding csv upload test 1

--- testing-app/tests/testthat/test-shinytest2.R
+++ testing-app/tests/testthat/test-shinytest2.R
@@ -4,7 +4,7 @@ test_that("{shinytest2} recording: one-name-greeting", {
   app <- AppDriver$new(name = "one-name-greeting", height = 819, width = 747)
   app$set_inputs(name_input = "Anna")
   app$click("greeting_button_input")
-  app$expect_values()
+  app$expect_values(output = "greeting_output")
 })
 
 
@@ -15,14 +15,14 @@ test_that("{shinytest2} recording: consecutive-name-greeting", {
   app$click("greeting_button_input")
   app$set_inputs(name_input = "Anna")
   app$click("greeting_button_input")
-  app$expect_values()
+  app$expect_values(output = "greeting_output")
 })
 
 
 test_that("{shinytest2} recording: no-name-greeting", {
   app <- AppDriver$new(name = "no-name-greeting", height = 819, width = 747)
   app$click("greeting_button_input")
-  app$expect_values()
+  app$expect_values(output = "greeting_output")
 })
 
 
@@ -30,9 +30,16 @@ test_that("{shinytest2} recording: consecutive-name-greetings", {
   app <- AppDriver$new(name = "consecutive-name-greetings", height = 819, width = 747)
   app$set_inputs(name_input = "Anna")
   app$click("greeting_button_input")
-  app$expect_values()
+  app$expect_values(output = "greeting_output")
   app$set_inputs(name_input = "Ama")
   app$set_inputs(name_input = "Amanda")
   app$click("greeting_button_input")
-  app$expect_values()
+  app$expect_values(output = "greeting_output")
+})
+
+
+test_that("{shinytest2} recording: upload-cols-and-data", {
+  app <- AppDriver$new(name = "upload-cols-and-data", height = 819, width = 747)
+  app$upload_file(csv_input = "cols-and-data1.csv")
+  app$expect_values(output = "summary_table_output")
 })
