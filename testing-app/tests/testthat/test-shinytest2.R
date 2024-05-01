25b7f3460f57ff7641ed301c33d57170411d9bec
annaramji <anna@ramji.org>
2024-05-01 14:11:04 -0700

    practiced breaking, failing, reviewing, accepting tests from changing testing-app

--- testing-app/tests/testthat/test-shinytest2.R
+++ testing-app/tests/testthat/test-shinytest2.R
@@ -24,3 +24,15 @@ test_that("{shinytest2} recording: no-name-greeting", {
   app$click("greeting_button_input")
   app$expect_values()
 })
+
+
+test_that("{shinytest2} recording: consecutive-name-greetings", {
+  app <- AppDriver$new(name = "consecutive-name-greetings", height = 819, width = 747)
+  app$set_inputs(name_input = "Anna")
+  app$click("greeting_button_input")
+  app$expect_values()
+  app$set_inputs(name_input = "Ama")
+  app$set_inputs(name_input = "Amanda")
+  app$click("greeting_button_input")
+  app$expect_values()
+})
