64825c5e7bb9acf3098fd690161bb5e4f1750873
annaramji <anna@ramji.org>
2024-05-01 13:52:12 -0700

    created testing app, ran tests (passed)

---
+++ testing-app/tests/testthat/test-shinytest2.R
@@ -0,0 +1,26 @@
+library(shinytest2)
+
+test_that("{shinytest2} recording: one-name-greeting", {
+  app <- AppDriver$new(name = "one-name-greeting", height = 819, width = 747)
+  app$set_inputs(name_input = "Anna")
+  app$click("greeting_button_input")
+  app$expect_values()
+})
+
+
+test_that("{shinytest2} recording: consecutive-name-greeting", {
+  app <- AppDriver$new(name = "consecutive-name-greeting", height = 819, width = 747)
+  app$set_inputs(name_input = "A")
+  app$set_inputs(name_input = "Amanda")
+  app$click("greeting_button_input")
+  app$set_inputs(name_input = "Anna")
+  app$click("greeting_button_input")
+  app$expect_values()
+})
+
+
+test_that("{shinytest2} recording: no-name-greeting", {
+  app <- AppDriver$new(name = "no-name-greeting", height = 819, width = 747)
+  app$click("greeting_button_input")
+  app$expect_values()
+})
